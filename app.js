1d83062938b1c29d9c39064316be75cb517ed801
Anubha Kansal <anubha.18jics074@jietjodhpur.ac.in>
2020-06-17 15:25:25 +0530

    Classwork : Search bar Functionality added

---
+++ app.js
@@ -0,0 +1,65 @@
+//Delete a movie
+
+const list = document.querySelector('#movie-list ul');
+
+list.addEventListener('click', (e) => {
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        // li.parentNode.removeChild(li); 
+        list.removeChild(li);
+    }
+})
+
+//Add a new Movie
+
+const addMovieForm = document.forms['add-movie'];
+
+addMovieForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const value = addMovieForm.querySelector("input[type='text']").value;
+
+    console.log(value);
+    const li = document.createElement('li');
+    const movieName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+    deleteBtn.textContent = 'delete';
+    movieName.textContent = value;
+    
+    deleteBtn.classList.add('delete')
+    movieName.classList.add('name');
+    li.appendChild(movieName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+});
+
+// Hide List
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', () => {
+    console.log(hideBox.checked);
+    if(hideBox.checked){
+        list.style.display = 'none';
+    }else{
+        list.style.display = 'inline';
+    }
+});
+
+//Search 
+// Task - Search the movie from the list provided:
+// 1. As the user types, the list should show the matching pattern movies
+// 2. If the movie is not the part of our list then we show nothing as long as the search box has values
+
+const searchMovieForm = document.forms['page-banner'];
+console.log(searchMovieForm);
+
+searchMovieForm.addEventListener('keyup', () => {
+    let movieList = document.querySelectorAll('#movie-list ul li');
+    
+    movieList.forEach( movie => {
+        if(! movie.querySelector('.name').textContent.toLowerCase().includes(searchMovieForm.querySelector('input').value.toLowerCase())){
+            movie.style.display = 'none';
+        }else{
+            movie.style.display = 'block';
+        }
+    }
+    );
+});
